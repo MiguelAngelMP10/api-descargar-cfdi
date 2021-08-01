b85d73639c86649af62e106ab11180fd6fb7fc1e
Carlos C Soto <eclipxe13@gmail.com>
2021-08-01 00:40:09 -0500

    Add helper class that shares logic in controllers

---
+++ app/Helpers/SatWsService.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Helpers;
+
+use Exception;
+use Illuminate\Support\Facades\Storage;
+use PhpCfdi\SatWsDescargaMasiva\RequestBuilder\FielRequestBuilder\Fiel;
+use PhpCfdi\SatWsDescargaMasiva\RequestBuilder\FielRequestBuilder\FielRequestBuilder;
+use PhpCfdi\SatWsDescargaMasiva\Service;
+use PhpCfdi\SatWsDescargaMasiva\Shared\ServiceEndpoints;
+use PhpCfdi\SatWsDescargaMasiva\WebClient\GuzzleWebClient;
+
+class SatWsService
+{
+    public function createService(string $rfc, string $password, bool $retenciones): Service
+    {
+        $contentCer = Storage::get($this->obtainCertificatePath($rfc));
+        $contentKey = Storage::get($this->obtainPrivateKeyPath($rfc));
+
+        $fiel = $this->createFiel($contentCer, $contentKey, $password);
+
+        // creación del web client basado en Guzzle que implementa WebClientInterface
+        // para usarlo necesitas instalar guzzlehttp/guzzle pues no es una dependencia directa
+        $webClient = new GuzzleWebClient();
+
+        // creación del objeto encargado de crear las solicitudes firmadas usando una FIEL
+        $requestBuilder = new FielRequestBuilder($fiel);
+
+        // Creación del servicio
+        $endpoints = (! $retenciones) ? ServiceEndpoints::cfdi() : ServiceEndpoints::retenciones();
+        return new Service($requestBuilder, $webClient, null, $endpoints);
+    }
+
+    public function createFiel(string $contentCer, string $contentKey, string $password): Fiel
+    {
+        $fiel = Fiel::create($contentCer, $contentKey, $password);
+
+        // verificar que la FIEL sea válida (no sea CSD y sea vigente acorde a la fecha del sistema)
+        if (! $fiel->isValid()) {
+            throw new Exception('La FIEL no es valida');
+        }
+
+        return $fiel;
+    }
+
+    public function obtainCertificatePath(string $rfc): string
+    {
+        return 'datos/' . $rfc . "/" . $rfc . '.cer';
+    }
+
+    public function obtainPrivateKeyPath(string $rfc): string
+    {
+        return 'datos/' . $rfc . "/" . $rfc . '.key';
+    }
+}
