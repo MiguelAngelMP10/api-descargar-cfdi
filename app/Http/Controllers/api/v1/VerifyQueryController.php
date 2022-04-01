0b8e31df8fd7083a76a0b3a79ce6e6481eeb120e
Miguel Ángel Muñoz Pozos <mmunozpozos@gmail.com>
2022-03-31 22:42:01 -0600

    refactor VerifyQueryController.php

--- app/Http/Controllers/api/v1/VerifyQueryController.php
+++ app/Http/Controllers/api/v1/VerifyQueryController.php
@@ -6,7 +6,6 @@ use App\Helpers\SatWsService;
 use App\Http\Controllers\Controller;
 use Exception;
 use Illuminate\Http\Request;
-use PhpCfdi\SatWsDescargaMasiva\Service;
 use PhpCfdi\SatWsDescargaMasiva\Services\Verify\VerifyResult;
 
 class VerifyQueryController extends Controller
@@ -14,21 +13,28 @@ class VerifyQueryController extends Controller
     private string $RFC;
     private string $password;
     private bool $retenciones;
-    private Service $service;
     private string $requestId;
 
     /**
      * @param Request $request
      *
-     * @return void
+     * @return \Illuminate\Http\JsonResponse|VerifyResult
      */
 
     public function verifyQuery(Request $request)
     {
         $this->getParamsRequest($request);
-        $this->createService();
-        $verify = $this->service->verify($this->requestId);
-        $this->evaluateResponse($verify);
+        $satWsServiceHelper = new SatWsService();
+        try {
+            $service = $satWsServiceHelper->createService(
+                $this->RFC,
+                $this->password,
+                $this->retenciones
+            );
+            return $service->verify($this->requestId);
+        } catch (Exception $exception) {
+            return response()->json(['message' => $exception->getMessage()], 422);
+        }
     }
 
     private function getParamsRequest(Request $request)
@@ -37,65 +43,5 @@ class VerifyQueryController extends Controller
         $this->password = $request->input('password');
         $this->retenciones = $request->boolean('retenciones');
         $this->requestId = $request->input('requestId');
-    }
-
-    private function createService(): void
-    {
-        $satWsServiceHelper = new SatWsService();
-        try {
-            $this->service = $satWsServiceHelper->createService(
-                $this->RFC,
-                $this->password,
-                $this->retenciones
-            );
-        } catch (Exception $exception) {
-            response()->json(['message' => $exception->getMessage()], 422);
-            return;
-        }
-    }
-
-    private function evaluateResponse(VerifyResult $verify): void
-    {
-        if (! $verify->getStatus()->isAccepted()) {
-            response()->json([
-                'message' => "Fallo al verificar la consulta {$this->requestId}: {$verify->getStatus()->getMessage()}",
-            ]);
-            return;
-        }
-
-        if (! $verify->getCodeRequest()->isAccepted()) {
-            response()->json([
-                'message' => "La solicitud {$this->requestId} fue rechazada: {$verify->getCodeRequest()->getMessage()}",
-            ]);
-            return;
-        }
-
-        $this->evaluateStatusRequest($verify);
-    }
-
-    private function evaluateStatusRequest(VerifyResult $verify)
-    {
-        $statusRequest = $verify->getStatusRequest();
-        if ($statusRequest->isExpired() || $statusRequest->isFailure() || $statusRequest->isRejected()) {
-            response()->json([
-                'message' => "La solicitud {$this->requestId} no se puede completar",
-            ]);
-            return;
-        }
-
-        if ($statusRequest->isInProgress() || $statusRequest->isAccepted()) {
-            response()->json([
-                'message' => "La solicitud {$this->requestId} se está procesando",
-            ]);
-            return;
-        }
-
-        if ($statusRequest->isFinished()) {
-            response()->json([
-                'message' => "La solicitud {$this->requestId} está lista",
-                'numPaquetes' => "Se encontraron {$verify->countPackages()} paquetes",
-                'packagesIds' => $verify->getPackagesIds(),
-            ]);
-        }
     }
 }
