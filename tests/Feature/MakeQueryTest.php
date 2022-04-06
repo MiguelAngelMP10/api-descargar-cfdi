c04f4bca80d4a54aefb492e07d93fb46c029ba82
Miguel Ángel Muñoz Pozos <mmunozpozos@gmail.com>
2022-04-05 21:11:12 -0500

    add test_validate_add_document_type_in, test_validate_add_complemento_cfdi

--- tests/Feature/MakeQueryTest.php
+++ tests/Feature/MakeQueryTest.php
@@ -53,19 +53,19 @@ class MakeQueryTest extends TestCase
             'message' => 'Invalid data',
             'errors' => [
                 'downloadType' => ['The downloadType must be one of the following types: issued, received'],
-                'requestType' => ['The requestType must be one of the following types: cfdi, metadata']
+                'requestType' => ['The requestType must be one of the following types: xml, metadata']
             ],
         ]);
     }
 
-    public function test_exception_create_sat_ws_service_helper(): void
+    public function test_validate_add_rfc_match_empty()
     {
         $this->sanctumAuthenticate();
         $response = $this->post(
             '/api/v1/make-query',
             [
-                'RFC' => 'EKU9003kk173C9',
-                'password' => "klkllkhuu",
+                'RFC' => "lo-que-sea",
+                'password' => "lo-que-sea",
                 "period" => [
                     "start" => "2021-11-01 00:00:01",
                     "end" => "2021-12-31 23:59:59"
@@ -76,10 +76,66 @@ class MakeQueryTest extends TestCase
                 "rfcMatch" => ""
             ]
         );
+
         $response->assertStatus(422)->assertJson([
-            'message' => 'The RFC expression does not contain the valid parts',
+            'message' => 'Invalid data',
+            'errors' => [
+                'rfcMatch' => ['The rfc match must be an array.']
+            ],
         ]);
     }
 
+    public function test_validate_add_document_type_in()
+    {
+        $this->sanctumAuthenticate();
+        $response = $this->post(
+            '/api/v1/make-query',
+            [
+                'RFC' => 'EKU9003173C9',
+                'password' => "12345678a",
+                "period" => [
+                    "start" => "2021-11-01 00:00:01",
+                    "end" => "2021-11-01 23:59:59"
+                ],
+                "retenciones" => false,
+                "downloadType" => "received",
+                "requestType" => "metadata",
+                "rfcMatch" => [],
+                "documentType" => "ALGO-QUE-NO"
+            ]
+        );
 
+        $response->assertStatus(422)->assertJson([
+            'message' => 'Invalid data',
+            'errors' => [
+                'documentType' => ["The documentType must be one of the following types: ingreso, I, egreso, E, traslado, T, nomina, N, pago, P"]
+            ]
+        ]);
+    }
+
+    public function test_validate_add_complemento_cfdi()
+    {
+        $this->sanctumAuthenticate();
+        $response = $this->post(
+            '/api/v1/make-query',
+            [
+                'RFC' => 'EKU9003173C9',
+                'password' => "12345678a",
+                "period" => [
+                    "start" => "2021-11-01 00:00:01",
+                    "end" => "2021-11-01 23:59:59"
+                ],
+                "retenciones" => false,
+                "downloadType" => "received",
+                "requestType" => "metadata",
+                "rfcMatch" => [],
+                "documentType" => "E",
+                "complementoCfdi" => "algo"
+            ]
+        );
+
+        $response->assertStatus(422)->assertJson([
+            'message' => "PhpCfdi\\SatWsDescargaMasiva\\Shared\\ComplementoCfdi value algo was not found"
+        ]);
+    }
 }
