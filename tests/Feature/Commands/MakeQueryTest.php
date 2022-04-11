f85c30334d6581c5260cdf841b2920370d2ae939
Miguel Ángel Muñoz Pozos <mmunozpozos@gmail.com>
2022-04-11 18:35:15 -0500

    add test_cer_and_key_empty_command in MakeQueryTest.php

--- tests/Feature/Commands/MakeQueryTest.php
+++ tests/Feature/Commands/MakeQueryTest.php
@@ -5,5 +5,15 @@ use Tests\TestCase;
 
 class MakeQueryTest extends TestCase
 {
+    private string $pathCer = "tests/_files/fake-fiel/EKU9003173C9.cer";
+    private string $pathKey = "tests/_files/fake-fiel/EKU9003173C9.key";
+    private string $password = '12345678a';
+
+    public function test_cer_and_key_empty_command()
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('Not enough arguments (missing: "cer, key").');
+        $this->artisan('sw:make:query ');
+    }
 
 }
