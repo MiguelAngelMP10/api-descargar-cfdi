71f3e3f1bba0fe99008eae37713c40a33957372f
Miguel Ángel Muñoz Pozos <mmunozpozos@gmail.com>
2022-04-13 23:55:24 -0500

    add command MakeQueryInteractive.php

--- app/Console/Commands/MakeQueryInteractive.php
+++ app/Console/Commands/MakeQueryInteractive.php
@@ -21,21 +21,11 @@ class MakeQueryInteractive extends Command
     protected $description = 'Command description';
 
     /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    /**
      * Execute the console command.
      *
      * @return int
      */
-    public function handle()
+    public function handle(): int
     {
         return 0;
     }
