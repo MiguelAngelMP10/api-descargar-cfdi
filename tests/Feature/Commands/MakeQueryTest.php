2d12cb3c3af961738f57aa232b5abbeec4402687
Miguel Ángel Muñoz Pozos <mmunozpozos@gmail.com>
2022-04-13 23:57:56 -0500

    add test test_password_required

--- tests/Feature/Commands/MakeQueryTest.php
+++ tests/Feature/Commands/MakeQueryTest.php
@@ -1,12 +1,18 @@
 <?php
+
 namespace Tests\Feature\Commands;
 
+use App\Utils\ComplementoCfdiList;
+use Symfony\Component\Console\Helper\TableCell;
+use Symfony\Component\Console\Helper\TableSeparator;
 use Tests\TestCase;
 
 class MakeQueryTest extends TestCase
 {
     private string $pathCer = "tests/_files/fake-fiel/EKU9003173C9.cer";
     private string $pathKey = "tests/_files/fake-fiel/EKU9003173C9.key";
+    private string $plainText = "tests/_files/plain-text.txt";
+
     private string $password = '12345678a';
 
     public function test_cer_and_key_empty_command()
@@ -16,4 +22,10 @@ class MakeQueryTest extends TestCase
         $this->artisan('sw:make:query ');
     }
 
+    public function test_password_required()
+    {
+        $this->artisan('sw:make:query ' . $this->pathCer . ' ' . $this->pathKey)
+            ->expectsOutput('The password field is required.')
+            ->assertFailed();
+    }
 }
