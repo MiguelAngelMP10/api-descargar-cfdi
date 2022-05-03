54eb196e71ace33872e6ad7f57ded58e4f01f30f
Miguel Ángel Muñoz Pozos <mmunozpozos@gmail.com>
2022-05-02 22:31:09 -0500

    add class WriteCatalogModelSat.php

---
+++ app/Helpers/WriteCatalogModelSat.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Support\Facades\Storage;
+
+class WriteCatalogModelSat
+{
+    public static function writeModel($nameClass, $nameTable): bool
+    {
+        $stringModel = "<?php
+
+namespace App\Models\Catalogs;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ${nameClass} extends Model
+{
+    public \$incrementing = false;
+    public \$timestamps = false;
+    protected \$connection = 'sqlite_catalogs';
+    protected \$table = '${nameTable}';
+}
+";
+        return Storage::disk('catalogs')
+            ->put($nameClass . '.php', $stringModel);
+    }
+}
