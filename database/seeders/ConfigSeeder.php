ff628184e649e9ed8f24c3aff3901493c3a0e193
Miguel Ángel Muñoz Pozos <mmunozpozos@gmail.com>
2022-05-02 22:11:15 -0500

    add Seeder ConfigSeeder.php

---
+++ database/seeders/ConfigSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ConfigSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('config')->insert([
+            'name' => 'resources-sat-catalogs-version',
+            'value' => '',
+        ]);
+    }
+}
