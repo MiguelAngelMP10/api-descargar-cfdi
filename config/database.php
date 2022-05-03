04d3964990530c2bb6501a0b4c82f94f59af31ff
Miguel Ángel Muñoz Pozos <mmunozpozos@gmail.com>
2022-05-02 22:24:46 -0500

    add config sqlite_catalogs in database.php

--- config/database.php
+++ config/database.php
@@ -43,6 +43,14 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+        'sqlite_catalogs' => [
+            'driver' => 'sqlite',
+            'url' => env('DATABASE_URL'),
+            'database' => env('DB_DATABASE_CATALOGS'),
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        ],
+
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
