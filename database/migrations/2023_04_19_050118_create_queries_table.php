62cee12cbf7a7679c8bc089af6bc844d18582749
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-04-24 20:15:37 -0600

    add migration queries

---
+++ database/migrations/2023_04_19_050118_create_queries_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Query\Expression;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('queries', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('rfc');
+            $table->enum('endPoint', ['cfdi', 'retenciones'])->default('cfdi');
+            $table->enum('downloadType', ['issued', 'received'])->default('issued');
+            $table->enum('requestType', ['xml', 'metadata'])->default('metadata');
+
+            $table->dateTime('dateTimePeriodStart');
+            $table->dateTime('dateTimePeriodEnd');
+            $table->uuid("requestId")->nullable();
+            $table->integer("numeroCFDIs")->nullable();
+
+            $table->enum('documentType', ['I', 'E', 'N', 'T', 'P'])->nullable();
+            $table->enum('documentStatus', ['active', 'cancelled'])->nullable();
+            $table->string('complementoCfdi')->nullable();
+            $table->json('rfcMatches')->default(new Expression('(JSON_ARRAY())'));
+            $table->uuid('uuid')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('queries');
+    }
+};
