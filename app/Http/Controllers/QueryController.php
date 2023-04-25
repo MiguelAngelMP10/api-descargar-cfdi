f922a701af4856d128e402b6780b4386279429fa
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-04-24 23:00:14 -0600

    add index

---
+++ app/Http/Controllers/QueryController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreQueryRequest;
+use App\Models\Query;
+use Illuminate\Http\Request;
+use Inertia\Response;
+use Inertia\ResponseFactory;
+
+class QueryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request): Response|ResponseFactory
+    {
+        $queries = Query::query()->when($request->search, function ($query, $search) {
+            $query->where('rfc', 'like', '%' . $search . '%')
+                ->orWhere('endPoint', 'like', '%' . $search . '%')
+                ->orWhere('downloadType', 'like', '%' . $search . '%')
+                ->orWhere('requestType', 'like', '%' . $search . '%');
+        })->paginate(10)->withQueryString();
+        return Inertia('Queries/Index', [
+            'queries' => $queries,
+            'search' => $request->search
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreQueryRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Query $query)
+    {
+        //
+    }
+
+}
