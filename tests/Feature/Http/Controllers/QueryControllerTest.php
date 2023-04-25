4e1786558a9d0b8e114e3ebdcafa374e579e74c1
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-04-24 22:56:32 -0600

    add test_index_display_a_listing_of_the_resource

---
+++ tests/Feature/Http/Controllers/QueryControllerTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use App\Models\Query;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Inertia\Testing\AssertableInertia as Assert;
+use Tests\TestCase;
+
+class QueryControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected User|Model $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->create();
+        Query::factory(10)->create();
+    }
+
+    public function test_index_display_a_listing_of_the_resource()
+    {
+        $this->actingAs($this->user)
+            ->get(route('queries.index'))
+            ->assertOk()
+            ->assertInertia(fn(Assert $page) => $page
+                ->component('Queries/Index')
+                ->has('queries')
+                ->has('search')
+                ->has('queries.links')
+                ->has('queries.data', 10)
+            );
+    }
+
+    public function test_show_the_form_for_creating_a_new_resource()
+    {
+
+    }
+
+    public function test_store_a_newly_created_resource_in_storage()
+    {
+
+    }
+
+    public function test_show_query_display_the_specified_resource()
+    {
+
+    }
+}
