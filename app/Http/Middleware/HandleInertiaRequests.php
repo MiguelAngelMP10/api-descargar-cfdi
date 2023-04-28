f416e1ebeafe994b86dc42a1a32e5f046d6d47d3
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-04-27 22:55:51 -0600

    update HandleInertiaRequests.php

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -41,6 +41,11 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
-        return array_merge(parent::share($request), []);
+        return array_merge(parent::share($request), [
+            'flash' => [
+                'success' => $request->session()->get('success'),
+                'error' => $request->session()->get('error'),
+            ],
+        ]);
     }
 }
