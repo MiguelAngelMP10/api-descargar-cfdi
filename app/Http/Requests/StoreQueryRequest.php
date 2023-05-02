458f83693b814d40c62fdf800114d9a395228897
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-05-02 01:30:13 -0600

    add StoreQueryRequest.php

--- app/Http/Requests/StoreQueryRequest.php
+++ app/Http/Requests/StoreQueryRequest.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Requests;
 
+use App\Rules\RfcValidRule;
+use App\Utils\ComplementoCfdiList;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreQueryRequest extends FormRequest
 {
@@ -12,7 +15,7 @@ class StoreQueryRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,6 +25,48 @@ class StoreQueryRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [];
+        return [
+            'period_start' => 'date',
+            'period_end' => 'date',
+            'endPoint' => ['array', 'nullable', Rule::in(['cfdi', 'retenciones'])],
+            'downloadType' => [
+                'array', Rule::in(['issued', 'received']),
+            ],
+            'requestType' => [
+                'array', Rule::in(['xml', 'metadata']),
+            ],
+            'rfcMatches' => 'array',
+            'documentType' => [
+                'nullable', 'string', Rule::in(['I', 'E', 'T', 'N', 'P']),
+            ],
+            'complementoCfdi' => [
+                'string',
+                'nullable',
+                Rule::in(array_keys(ComplementoCfdiList::COMPLEMENTOS_CFDI_LIST))
+            ],
+            'documentStatus' => [
+                'nullable', Rule::in(['active', 'cancelled']),
+            ],
+            'uuid' => ['nullable', 'uuid'],
+            'rfcOnBehalf' => ['nullable', 'string', new RfcValidRule()],
+            'rfcMatches.*' => ['nullable', 'string', new RfcValidRule()],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'password.required' => 'The password field is required.',
+            'password.string' => 'The password field no is string.',
+            'downloadType.in' => 'The downloadType must be one of the following types: :values',
+            'requestType.in' => 'The requestType must be one of the following types: :values',
+            'documentType.string' => 'The documentType must be a string.',
+            'documentType.in' => 'The documentType must be one of the following types: :values',
+            'complementoCfdi,in' => 'The complementoCfdi must be one of the following types: :values',
+            'endPoint.in' => 'The endPoint must be one of the following types: :values',
+            'documentStatus.string' => 'The documentStatus must be a string.',
+            'documentStatus.in' => 'The documentStatus must be one of the following types: :values',
+            'rfcMatches.array' => 'The rfcMatches must be an array.',
+        ];
     }
 }
