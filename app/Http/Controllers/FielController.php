fa1fea466a0c5ffccec2fa05f6a27d2aced6e19a
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-05-09 16:25:19 -0600

    add scaffolding the table Fiel

---
+++ app/Http/Controllers/FielController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreFielRequest;
+use App\Http\Requests\UpdateFielRequest;
+use App\Models\Fiel;
+
+class FielController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreFielRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Fiel $fiel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Fiel $fiel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateFielRequest $request, Fiel $fiel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Fiel $fiel)
+    {
+        //
+    }
+}
