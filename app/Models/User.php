bf6093e4c4482abb1a659e041a96056cef811096
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-05-09 16:28:48 -0600

    add Relations HasMany fiels

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
@@ -57,4 +58,10 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function fiels(): HasMany
+    {
+        return $this->hasMany(Fiel::class);
+    }
+
 }
