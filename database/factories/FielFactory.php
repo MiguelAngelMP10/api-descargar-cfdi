d03b894706aa3a0b75a844bb5c41f0cd6feeee84
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-05-09 20:20:05 -0600

    update FielFactory.php

--- database/factories/FielFactory.php
+++ database/factories/FielFactory.php
@@ -2,10 +2,13 @@
 
 namespace Database\Factories;
 
+use App\Models\Fiel;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Crypt;
+use PhpCfdi\Credentials\Credential;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Fiel>
+ * @extends Factory<Fiel>
  */
 class FielFactory extends Factory
 {
@@ -16,8 +19,29 @@ class FielFactory extends Factory
      */
     public function definition(): array
     {
+
+
+        $pathFakeFiel = base_path('tests/_files/fake-fiel/');
+
+        $certificatePath = $pathFakeFiel . 'EKU9003173C9-pem.cer';
+        $privateKeyPath = $pathFakeFiel . 'EKU9003173C9-pem.key';
+
+        $passPhrase = trim(file_get_contents($pathFakeFiel . 'EKU9003173C9-password.txt'));
+
+        $cer = file_get_contents($certificatePath);
+        $key = file_get_contents($privateKeyPath);
+
+        $fiel = Credential::create($cer, $key, $passPhrase);
+
+        $certificado = $fiel->certificate();
+
         return [
-            //
+            'user_id' => 1,
+            'rfc' => $certificado->rfc(),
+            'legalName' => $certificado->legalName(),
+            'cer' => Crypt::encryptString($cer),
+            'key' => Crypt::encryptString($key),
+            'password' => Crypt::encryptString($passPhrase),
         ];
     }
 }
