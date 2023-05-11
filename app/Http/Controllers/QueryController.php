05d04c820039774c2b89df989e9e0dc56e157315
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-05-10 23:20:01 -0600

    update QueryController.php

--- app/Http/Controllers/QueryController.php
+++ app/Http/Controllers/QueryController.php
@@ -4,14 +4,36 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreQueryRequest;
 use App\Models\Query;
+use App\Traits\AddParametersToQuery;
 use App\Utils\ComplementoCfdiList;
+use Carbon\Carbon;
+use Exception;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 use Inertia\Response;
 use Inertia\ResponseFactory;
+use PhpCfdi\SatWsDescargaMasiva\RequestBuilder\FielRequestBuilder\Fiel;
+use PhpCfdi\SatWsDescargaMasiva\RequestBuilder\FielRequestBuilder\FielRequestBuilder;
+use PhpCfdi\SatWsDescargaMasiva\Service;
+use PhpCfdi\SatWsDescargaMasiva\Services\Query\QueryParameters;
+use PhpCfdi\SatWsDescargaMasiva\Shared\DateTimePeriod;
+use PhpCfdi\SatWsDescargaMasiva\Shared\DownloadType;
+use PhpCfdi\SatWsDescargaMasiva\Shared\RequestType;
+use PhpCfdi\SatWsDescargaMasiva\Shared\ServiceEndpoints;
+use PhpCfdi\SatWsDescargaMasiva\WebClient\GuzzleWebClient;
 
 class QueryController extends Controller
 {
+    use AddParametersToQuery;
+
+    protected QueryParameters $queryParameters;
+
+    /**
+     * @var array
+     */
+    private array $rfcMatches;
+
     /**
      * Display a listing of the resource.
      */
@@ -49,7 +71,7 @@ class QueryController extends Controller
                     'P' => 'Pago',
                 ],
                 'complementoCfdi' => ComplementoCfdiList::COMPLEMENTOS_CFDI_LIST,
-                'documentStatus' => ['undefined' => 'Undefined','active' => 'Active', 'cancelled' => 'Cancelled'],
+                'documentStatus' => ['undefined' => 'Undefined', 'active' => 'Active', 'cancelled' => 'Cancelled'],
                 'fiels' => $request->user()->fiels()->get(),
             ]
         );
@@ -60,9 +82,72 @@ class QueryController extends Controller
      */
     public function store(StoreQueryRequest $request): RedirectResponse
     {
+        try {
+            $start = null;
+            $end = null;
 
-        dd( $request->all());
-        return redirect()->route('queries.create')->with('success', 'Query created Successfully');
+            if ($request->input('period_start') !== null && $request->input('period_end') !== null) {
+                $start = Carbon::createFromFormat('Y-m-d', $request->input('period_start'));
+                $end = Carbon::createFromFormat('Y-m-d', $request->input('period_end'));
+                $start->setHour(0)->setMinutes(0)->setSecond(0)->toDate();
+                $end->setHour(23)->setMinutes(59)->setSecond(59)->toDate();
+                $start = $start->format('Y-m-d H:i:s');
+                $end = $end->format('Y-m-d H:i:s');
+            }
+
+            $fielDB = $request->user()->fiels()->where('rfc', $request->input('rfc'))->first();
+
+            $webClient = new GuzzleWebClient();
+            $fiel = Fiel::create(
+                Crypt::decryptString($fielDB->cer),
+                Crypt::decryptString($fielDB->key),
+                Crypt::decryptString($fielDB->password)
+            );
+
+            $requestBuilder = new FielRequestBuilder($fiel);
+
+            foreach ($request->input('endPoint') as $endPoint) {
+                foreach ($request->input('downloadType') as $downloadType) {
+                    foreach ($request->input('requestType') as $requestType) {
+                        $service = new Service($requestBuilder, $webClient, null, $this->getEndpoints($endPoint));
+                        $this->queryParameters = QueryParameters::create()
+                            ->withPeriod(DateTimePeriod::createFromValues($start, $end))
+                            ->withDownloadType($this->getDownloadType($downloadType))
+                            ->withRequestType($this->getRequestType($requestType));
+
+                        $this->addDocumentTypeToQueryParameters($request);
+                        $this->addComplementoCfdi($request);
+                        $this->addDocumentStatus($request);
+                        $this->addUuid($request);
+                        $this->addRfcOnBehalf($request);
+                        $this->rfcMatches = $request->has('rfcMatches') ? $request->input('rfcMatches') : [];
+                        $this->addRfcMatches();
+
+                        $query = $service->query($this->queryParameters);
+                        $request->user()->queries()->create([
+                            'rfc' => $request->input('rfc'),
+                            'endPoint' => $endPoint,
+                            'downloadType' => $downloadType,
+                            'requestType' => $requestType,
+                            'dateTimePeriodStart' => $this->queryParameters->getPeriod()->getStart()->format('Y-m-d H:i:s'),
+                            'dateTimePeriodEnd' => $this->queryParameters->getPeriod()->getEnd()->format('Y-m-d H:i:s'),
+                            'requestId' => $query->getRequestId(),
+                            'documentType' => $this->queryParameters->getDocumentType()->value(),
+                            'documentStatus' => $this->queryParameters->getDocumentStatus()->value(),
+                            'complementoCfdi' => $this->queryParameters->getComplement()->value(),
+                            'rfcMatches' => json_encode($this->queryParameters->getRfcMatches()),
+                            'rfcOnBehalf' => $this->queryParameters->getRfcOnBehalf()->getValue(),
+                            'uuid' => $this->queryParameters->getUuid()->getValue(),
+                            'statusCode' => $query->getStatus()->getCode(),
+                            'statusMessage' => $query->getStatus()->getMessage(),
+                        ]);
+                    }
+                }
+            }
+            return redirect()->route('queries.create')->with('success', 'Queries created successfully');
+        } catch (Exception $exception) {
+            return redirect()->route('queries.create')->with('error', $exception->getMessage());
+        }
     }
 
     /**
@@ -73,5 +158,35 @@ class QueryController extends Controller
         return Inertia('Queries/Show', [
             'query' => $query,
         ]);
+    }
+
+    private function getEndpoints($endPoint)
+    {
+        if ($endPoint === 'cfdi') {
+            return ServiceEndpoints::cfdi();
+        }
+        if ($endPoint === 'retenciones') {
+            return ServiceEndpoints::retenciones();
+        }
+    }
+
+    private function getDownloadType($downloadType)
+    {
+        if ($downloadType === 'issued') {
+            return DownloadType::issued();
+        }
+        if ($downloadType === 'received') {
+            return DownloadType::received();
+        }
+    }
+
+    private function getRequestType($requestType)
+    {
+        if ($requestType === 'xml') {
+            return RequestType::xml();
+        }
+        if ($requestType === 'metadata') {
+            return RequestType::metadata();
+        }
     }
 }
