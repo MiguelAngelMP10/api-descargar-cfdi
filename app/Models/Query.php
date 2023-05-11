bbf8a330f80c7a6b61e6f24086d6b5c9e0225377
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-05-10 23:19:51 -0600

    update Query.php

--- app/Models/Query.php
+++ app/Models/Query.php
@@ -4,8 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Query extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'rfc',
+        'endPoint',
+        'downloadType',
+        'requestType',
+        'dateTimePeriodStart',
+        'dateTimePeriodEnd',
+        'requestId',
+        'numeroCFDIs',
+        'documentType',
+        'documentStatus',
+        'complementoCfdi',
+        'rfcMatches',
+        'rfcOnBehalf',
+        'uuid',
+        'statusCode',
+        'statusMessage',
+    ];
+
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class);
+    }
 }
