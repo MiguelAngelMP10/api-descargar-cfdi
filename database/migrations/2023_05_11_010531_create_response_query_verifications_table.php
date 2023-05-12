5815792cca7771c52c3621d0e2588e9c4274b6d1
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-05-11 18:02:13 -0600

    update 2023_05_11_010531_create_response_query_verifications_table.php

--- database/migrations/2023_05_11_010531_create_response_query_verifications_table.php
+++ database/migrations/2023_05_11_010531_create_response_query_verifications_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,6 +13,8 @@ return new class extends Migration
         Schema::create('response_query_verifications', function (Blueprint $table) {
             $table->id();
             $table->foreignId('query_id')->constrained();
+            $table->string('statusCode')->nullable();
+            $table->string('statusMessage')->nullable();
             $table->string('statusRequestMessage')->nullable();
             $table->string('statusRequestName')->nullable();
             $table->integer('statusRequestEntryIndex')->nullable();
