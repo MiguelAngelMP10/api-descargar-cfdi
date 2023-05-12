7c01f5e3549393aa93f0400953a86b5fe4c7442f
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-05-11 21:36:44 -0600

    add The attributes that are mass assignable.

--- app/Models/ResponseQueryVerification.php
+++ app/Models/ResponseQueryVerification.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class ResponseQueryVerification extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string>
+     */
+    protected $fillable = [
+        'query_id',
+        'statusCode',
+        'statusMessage',
+        'statusRequestMessage',
+        'statusRequestName',
+        'statusRequestEntryIndex',
+        'codeRequestValue',
+        'codeRequestName',
+        'codeRequestMessage',
+    ];
 }
