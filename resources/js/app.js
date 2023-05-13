5f7032cf462c5a7355d850cff5f90af39693703b
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-05-12 21:42:30 -0600

    add vue-json-viewer

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,14 @@ import './bootstrap';
 import '../css/app.css';
 import 'flowbite';
 
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import {createApp, h} from 'vue';
+import {createInertiaApp} from '@inertiajs/vue3';
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
+import {ZiggyVue} from '../../vendor/tightenco/ziggy/dist/vue.m';
 import ToastPlugin from 'vue-toast-notification';
 import 'vue-toast-notification/dist/theme-bootstrap.css';
+import 'vue-json-viewer/style.css'
+import JsonViewer from 'vue-json-viewer'
 
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
@@ -15,11 +17,12 @@ const appName = window.document.getElementsByTagName('title')[0]?.innerText || '
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
-    setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
+    setup({el, App, props, plugin}) {
+        return createApp({render: () => h(App, props)})
             .use(plugin)
             .use(ZiggyVue, Ziggy)
             .use(ToastPlugin)
+            .use(JsonViewer)
             .mount(el);
     },
     progress: {
