e843fac0e6d0a38d0b3354040ed75a7fc0ff8b96
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-06-08 00:38:00 -0600

    update CceClavesPedimentosControllerTest.php

--- tests/Feature/Http/Controllers/api/Catalogs/CceClavesPedimentosControllerTest.php
+++ tests/Feature/Http/Controllers/api/Catalogs/CceClavesPedimentosControllerTest.php
@@ -2,37 +2,18 @@
 
 namespace Tests\Feature\Http\Controllers\api\Catalogs;
 
-use App\Http\Controllers\api\Catalogs\CceClavesPedimentosController;
 use App\Models\Catalogs\CceClavesPedimentos;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
-use Illuminate\Support\Facades\Cache;
 
 class CceClavesPedimentosControllerTest extends TestCase
 {
     use RefreshDatabase;
 
+
     public function test_cce_claves_pedimentos_index(): void
     {
         $this->sanctumAuthenticate();
-        Http::fake([
-            'api.com/api/v1/catalogs/cce-claves-pedimentos' => Http::response([
-                'data' => [],
-                'links' => [
-                    'first',
-                    'last',
-                    'prev',
-                    'next'
-                ],
-                'meta' => [
-                    'current_page',
-                    'from',
-                    'last_page',
-                    'links',
-                ]]),
-        ]);
-
         $response = $this->get('/api/v1/catalogs/cce-claves-pedimentos');
         $response->assertStatus(200);
         $response->assertJsonStructure([
@@ -56,13 +37,6 @@ class CceClavesPedimentosControllerTest extends TestCase
     public function test_cce_claves_pedimentos_search(): void
     {
         $this->sanctumAuthenticate();
-        Http::fake([
-            'api.com/api/v1/catalogs/cce-claves-pedimentos/search' => Http::response([
-                'data' => [],
-                'links' => [],
-                'meta' => []
-            ]),
-        ]);
         $response = $this->postJson('/api/v1/catalogs/cce-claves-pedimentos/search', ['filters' => [
             [
                 'field' => 'id',
@@ -82,12 +56,9 @@ class CceClavesPedimentosControllerTest extends TestCase
     public function test_cce_claves_pedimentos_show(): void
     {
         $this->sanctumAuthenticate();
-        Http::fake([
-            'api.com/api/v1/catalogs/cce-claves-pedimentos/*' => Http::response([
-                'data' => [],
-            ]),
-        ]);
-        $response = $this->get('/api/v1/catalogs/cce-claves-pedimentos/a');
+        $model = CceClavesPedimentos::first();
+        $keyName = $model->getKeyName();
+        $response = $this->get('/api/v1/catalogs/cce-claves-pedimentos/'.$model->{$keyName});
         $response->assertStatus(200);
         $response->assertJsonStructure([
             'data' => []
